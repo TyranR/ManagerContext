6022e0470416370f618ecfc57334caf3b0525462
me@tyran.ru <Defender2018for>
2020-01-28 00:23:34 +0300

    ver 1

---
+++ tests/test_main.py
@@ -0,0 +1,39 @@
+import unittest
+import json
+from main import people
+from main import add
+
+
+def setUpModule():
+    test_documents = []
+    test_directories = {}
+
+
+class TestSecretaryProgram(unittest.TestCase):
+
+    def setUp(self):
+        self.test_documents = []
+        with open('fixtures/documents.json', 'r', encoding='utf-8') as \
+                out_docs:
+            self.test_documents.extend(json.load(out_docs))
+        self.test_directories = {}
+        with open('fixtures/directories.json', 'r', encoding='utf-8') as \
+                out_dirs:
+            self.test_directories.update(json.load(out_dirs))
+
+    def tearDown(self):
+        pass
+
+    def test_people_check(self):
+        pass
+
+    def test_add_document(self):
+        pass
+
+    def test_something(self):
+        # self.assertEqual(True, False)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
